6a31eebf24620cb73697c661507e8fe2edab9b56
geudes <geudes@student.42.fr>
2023-06-10 11:13:51 +0200

    Fixed a segfault

--- src/tree/opp.c
+++ src/tree/opp.c
@@ -6,7 +6,7 @@
 /*   By: geudes <geudes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 22:56:53 by geudes            #+#    #+#             */
-/*   Updated: 2023/06/09 09:01:31 by geudes           ###   ########.fr       */
+/*   Updated: 2023/06/10 11:13:13 by geudes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,6 @@ t_lexer *look_for_mid_opp(t_lexer *lexer, t_lexer *end_lexer)
         return (0);
     mid = look_for_last_opp(lexer, end_lexer);
     if (!mid)
-        mid = look_for_last_opp(lexer->next, end_lexer);
+        mid = look_for_mid_opp(lexer->next, end_lexer);
     return (mid);     
 }
