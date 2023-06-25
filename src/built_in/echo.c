c52b53fac4cba18f9da6977d813d1fc07fbce0af
geudes <geudes@student.42.fr>
2023-06-25 09:04:41 +0200

    fixed echo and added write error on echo

--- src/built_in/echo.c
+++ src/built_in/echo.c
@@ -6,40 +6,61 @@
 /*   By: geudes <geudes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 08:44:42 by geudes            #+#    #+#             */
-/*   Updated: 2023/06/25 04:01:31 by geudes           ###   ########.fr       */
+/*   Updated: 2023/06/25 09:02:41 by geudes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-// Returns 0 on succes 1 on error
-int	bi_echo(t_lcmd *lcmd, t_env *env)
+static int	get_option(t_lcmd *lcmd, int *dashn)
 {
 	int	i;
-	int	dashn;
 	int	j;
 
-	i = ((void)env, 0);
+	i = 0;
 	while (lcmd->cmd[++i])
 	{
 		j = 0;
-		if (lcmd->cmd[i][0] == '-')
+		if (lcmd->cmd[i][j] == '-')
 		{
 			while (lcmd->cmd[i][++j] && lcmd->cmd[i][j] == 'n')
-				dashn = 1;
+				if (!*dashn)
+					*dashn = 1;
 			if (lcmd->cmd[i][j])
 				break ;
 		}
 		else
 			break ;
 	}
+	return (i);
+}
+
+static int	echo_error(int fd_error)
+{
+	write(fd_error, "echo: write error: no space left on device\n", 43);
+	return (1);
+}
+
+// Returns 0 on succes 1 on error
+int	bi_echo(t_lcmd *lcmd, t_env *env)
+{
+	int	i;
+	int	dashn;
+
+	(void)env;
+	dashn = 0;
+	i = get_option(lcmd, &dashn);
 	while (lcmd->cmd[i])
 	{
-		write(lcmd->output, lcmd->cmd[i], ft_strlen(lcmd->cmd[i]));
+		if (write(lcmd->output, lcmd->cmd[i], ft_strlen(lcmd->cmd[i]))
+			!= (int)ft_strlen(lcmd->cmd[i]))
+			return (echo_error(lcmd->error));
 		if (lcmd->cmd[++i])
-			write(lcmd->output, " ", 1);
+			if (write(lcmd->output, " ", 1) != 1)
+				return (echo_error(lcmd->error));
 	}
 	if (!dashn)
-		write(lcmd->output, "\n", 1);
+		if (write(lcmd->output, "\n", 1) != 1)
+			return (echo_error(lcmd->error));
 	return (0);
 }
--- src/exec/get_builtin.c
+++ src/exec/get_builtin.c
@@ -3,46 +3,46 @@
 #define BUILTINSPLIT 3
 
 static char *builtin[] = {"cd", "export", "unset",
-    "pwd", "echo", "env", "exit"};
+	"pwd", "echo", "env", "exit"};
 
 static int (*bi_func[])() = {bi_cd, bi_export, bi_unset,
-    bi_pwd, bi_echo, bi_env};
+	bi_pwd, bi_echo, bi_env};
 
 int ft_strcmp(char *str1, char *str2)
 {
-    while (*str1 && *str2 && (unsigned char)*str1 == (unsigned char)*str2)
-    {
-        str1++;
-        str2++;
-    }
-    return ((unsigned char)*str1 - (unsigned char)*str2);
+	while (*str1 && *str2 && (unsigned char)*str1 == (unsigned char)*str2)
+	{
+		str1++;
+		str2++;
+	}
+	return ((unsigned char)*str1 - (unsigned char)*str2);
 }
 
 void ft_exec_builtin(t_lcmd *cmd, t_env *env, int nbr)
 {
-    if (nbr == 0)
-        g_cmd_exit = (bi_func[nbr])(cmd, env);
-    else if (nbr > 0 && nbr <= BUILTINSPLIT)
-        g_cmd_exit = (bi_func[nbr])(cmd, &env);
-    else if (nbr > BUILTINSPLIT && nbr < BUILTINNBR - 1)
-        g_cmd_exit = (bi_func[nbr])(cmd, env);
-    else if (nbr == BUILTINNBR - 1)
-        bi_exit();
+	if (nbr == 0)
+		g_cmd_exit = (bi_func[nbr])(cmd, env);
+	else if (nbr > 0 && nbr <= BUILTINSPLIT)
+		g_cmd_exit = (bi_func[nbr])(cmd, &env);
+	else if (nbr > BUILTINSPLIT && nbr < BUILTINNBR - 1)
+		g_cmd_exit = (bi_func[nbr])(cmd, env);
+	else if (nbr == BUILTINNBR - 1)
+		bi_exit();
 }
 
 int ft_is_builtin(t_lcmd *cmd, t_env *env)
 {
-    int i = 0;
+	int i = 0;
 
-    while (i < BUILTINNBR)
-    {
-        if (cmd->cmd && ft_strcmp(*(cmd->cmd), builtin[i]) == 0)
-        {
-            ft_exec_builtin(cmd, env, i);
-            return (1);
-            break ;
-        }
-        i++;
-    }
-    return (0);
+	while (i < BUILTINNBR)
+	{
+		if (cmd->cmd && ft_strcmp(*(cmd->cmd), builtin[i]) == 0)
+		{
+			ft_exec_builtin(cmd, env, i);
+			return (1);
+			break ;
+		}
+		i++;
+	}
+	return (0);
 }
