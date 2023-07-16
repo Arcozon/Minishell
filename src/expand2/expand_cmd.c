a751c46dee90e36fa6999452889cac8482a1e45e
geudes <geudes@student.42.fr>
2023-07-16 21:53:52 +0200

    fixed by a silly little clown

--- src/expand2/expand_cmd.c
+++ src/expand2/expand_cmd.c
@@ -6,7 +6,7 @@
 /*   By: geudes <geudes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 05:02:04 by geudes            #+#    #+#             */
-/*   Updated: 2023/07/16 09:52:49 by geudes           ###   ########.fr       */
+/*   Updated: 2023/07/16 21:42:14 by geudes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,20 @@ void	expand_cmd_ioe(t_lcmd *lcmd, t_minishell *ms)
 	while (*root && *root != lcmd->end_lexer)
 	{
 		if ((*root)->type == TEXT_SQ)
-		{
 			expand_sq(*root, ms);
-			root = &((*root)->next);
-		}
 		else if ((*root)->type == TEXT_DQ)
-		{
 			expand_dq(*root, ms);
-			root = &((*root)->next);
-		}
 		else if ((*root)->type == TEXT)
+		{
 			root = expand_text(root, ms);
-		else
-			root = &((*root)->next);
+			continue ;
+		}
+		root = &((*root)->next);
 	}
 	change_text(lcmd->start_lexer, lcmd->end_lexer);
 	change_text_into_cmd_args(lcmd->start_lexer, lcmd->end_lexer);
 	lcmd->cmd = create_cmd(lcmd->start_lexer, lcmd->end_lexer, ms);
 	lcmd->ioe_put = create_ioeput(lcmd->start_lexer, lcmd->end_lexer, ms);
+	aff_lexer(lcmd->start_lexer, 0);
+	write(2, "\n", 1);
 }
--- src/expand2/text.c
+++ src/expand2/text.c
@@ -6,7 +6,7 @@
 /*   By: geudes <geudes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 01:08:20 by geudes            #+#    #+#             */
-/*   Updated: 2023/07/16 19:56:18 by geudes           ###   ########.fr       */
+/*   Updated: 2023/07/16 21:32:17 by geudes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,16 @@ static t_lexer	**expand_text2(t_lexer **root, t_minishell *ms)
 	t_lexer	**return_value;
 	t_lexer	*my_litle_slly_trash;
 	t_lexer	*next;
+	t_lexer **ms_root;
+
+	ms_root = &(ms->lexer);
+	while (*ms_root && *ms_root != *root)
+		ms_root = &((*ms_root)->next);
 
 	next = (*root)->next;
 	my_litle_slly_trash = *root;
 	*root = expand_wc_v2((*root)->content, ms);
+	*ms_root = *root;
 	my_litle_slly_trash->next = 0;
 	lexer_add_back(&(ms->trash_wc), my_litle_slly_trash);
 	return_value = lexer_get_last(root);
